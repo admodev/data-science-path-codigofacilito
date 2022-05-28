227352ce4885196a8fff72f64ec591e1b04cad14
admodev <admodevcodes@outlook.com>
2022-05-27 21:22:03 -0300

    ipython basics

---
+++ src/main.py
@@ -0,0 +1,13 @@
+def suma(a: int, b: int) -> int:
+    """Returns the sum of two integers
+
+    Args:
+        a (int): N1
+        b (int): N2
+
+    Returns:
+        int: The sum of a + b"""
+    return a + b
+
+print(suma(20, 25))
+print(suma(30, 75))
---
+++ src/operaciones.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+def resta(a: int, b: int) -> int:
+    """Returns the substraction of two integers"""
+    return a - b
+    
+resultado = resta(20, 10)
+print(resultado)
+def suma(a: int, b: int) -> int:
+    """Returns the sum of two integers"""
+    return a + b
+    
+resultadoSuma = suma(10, 20)
+print(resultadoSuma)
